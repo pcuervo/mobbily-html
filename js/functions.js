bf3f314eb1dd4c36f3700e8ac40feb4563da5920
Chirag Sharma <chirag@pcuervo.com>
2015-05-14 11:37:54 -0500

    actiom buttons

--- js/functions.js
+++ js/functions.js
@@ -39,32 +39,24 @@ function getHeaderHeight(){
 	#Triggered events
 \*------------------------------------*/
 
-function fixedHeader(){
+function toggleActionButtons(){
 	//Get the header height so we can now when
 	//to change the heade state
 	var headerHeight = getHeaderHeight();
 	//Scrolled pixels in Y axis
 	var sy = scrollY();
-	//Compare the two numbers, when they are the same of less
+	//Compare the two numbers, when they are the same or less
 	//add fixed class to the header.
 	if ( sy >= headerHeight ) {
-		//Get the window height so we now how to position
-		//the header at the bottom
-		var windowHeight = $(window).outerHeight();
-		//Substract the header height from the window height
-		//and apply it as its top
-		var topHeader =  windowHeight - headerHeight;
-		$('.header-wrapper').addClass('header-wrapper--fixed').css('top', topHeader);
-		//setHeaderHeightPadding('.footer-wrapper', 'bottom');
+		$('.action-buttons').addClass('opened');
 	} else {
-		$('.header-wrapper').removeClass('header-wrapper--fixed').css('top', 0);
-		//setPadding('.footer-wrapper', 'bottom', 0);
+		$('.action-buttons').removeClass('opened');
 	}
 }
 
 //Get the scrolled pixels in Y axis
 function scrollY() {
-	return $('.content-wrapper').scrollTop();
+	return $(window).scrollTop();
 }
 
 
